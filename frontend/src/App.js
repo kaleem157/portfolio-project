98f4689912467cd0333eedf6e77a469e31270b0b
kaleem157 <skkaleemuddin73@gmail.com>
2025-02-09 21:40:10 +0530

    Setup React Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+function App(){
+  return(
+    <div>
+      <h1>
+        Welcome to My Portfolio Project
+      </h1>
+      <p>
+        This is the React frontend of the Project!
+      </p>
+    </div>
+  );
+}
+export default App;
